c6bea8e0592c9cc1a9df75eb3078842f74845d5c
jon-1 <jmm894@gmail.com>
2016-01-17 00:12:30 -0500

    Positioning with timer works - one color, no sound

--- nback5d/ActiveGameViewController.m
+++ nback5d/ActiveGameViewController.m
@@ -41,6 +41,7 @@
 @property (assign, nonatomic) CGFloat marginSize;
 @property (assign, nonatomic) CGFloat fullWidth;
 
+
 @property (assign, nonatomic) CGFloat globalTimer;
 
 @property (strong, nonatomic) UIButton* soundButton;
@@ -48,22 +49,28 @@
 @property (strong, nonatomic) UIButton* spaceButton;
 @property (strong, nonatomic) UIButton* symbolButton;
 
-
+@property (assign, nonatomic) NSInteger currentRegion;
+@property (strong, nonatomic) NSString* cellIdentifier;
 @end
 
 @implementation ActiveGameViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    
+
+    
     _marginSize = 10.0;
     _fullWidth = CGRectGetWidth(self.view.frame) - 2 * _marginSize;
     _flowLayout = [UICollectionViewFlowLayout new];
+
     _globalTimer = 0.0;
     
     UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]
                                    initWithTarget:self
                                    action:@selector(tapped:)];
-    
+    self.gridAmount = 9;
     [self.view addGestureRecognizer:tap];
     
     [self establishView];
@@ -78,12 +85,22 @@
         // again, point.x and point.y have the coordinates
     }
 }
+-(CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section {
+    return 0;
+}
+
+-(CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section{
+    return 0;
+}
 
 - (void)establishView {
     _topView = [[UIView alloc] initWithFrame:CGRectMake(_marginSize, _marginSize, _fullWidth, CGRectGetHeight(self.view.frame) * .15)];
     _gridCollectionView = [[UICollectionView alloc] initWithFrame: CGRectMake(_marginSize, CGRectGetHeight(_topView.frame) + _marginSize * 2, _fullWidth, CGRectGetHeight(self.view.frame) - 2 * CGRectGetHeight(_topView.frame) - 2 * _marginSize) collectionViewLayout:_flowLayout];
     _bottomView = [[UIView alloc] initWithFrame:CGRectMake(_marginSize, CGRectGetHeight(_topView.frame) + CGRectGetHeight(_gridCollectionView.frame) + _marginSize * 3, _fullWidth, CGRectGetHeight(self.view.frame) * .15 - _marginSize*2)];
-    
+    _cellIdentifier = @"cellIdentifier";
+    [_gridCollectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:_cellIdentifier];
+    _gridCollectionView.dataSource = self;
+    _gridCollectionView.delegate = self;
     _topView.backgroundColor = WHITE;
     _gridCollectionView.backgroundColor = WHITE;
     _gridCollectionView.layer.borderWidth = 1.0;
@@ -111,10 +128,8 @@
     [_timerProgressLabel setTrackColor:GRAY_P];
     [_timerProgressLabel setProgressColor:RED_PEPSI];
     [_timerProgressLabel setFont:[UIFont fontWithName:@"BebasNeueBold" size:55]];
-
-    NSTimer *timer;
     
-    timer = [NSTimer scheduledTimerWithTimeInterval: 1.0
+    _timer = [NSTimer scheduledTimerWithTimeInterval: 1.0
                                              target: self
                                            selector: @selector(fillTimer:)
                                            userInfo: nil
@@ -129,64 +144,37 @@
     [_soundButton setColor:LIGHT_GRAY_1 forState:UIControlStateNormal];
     [_soundButton setColor:LIGHT_GRAY_1 forState:UIControlStateHighlighted];
     [_soundButton setColor:GRAY_P forState:UIControlStateSelected];
-    [_soundButton addTarget:self action:@selector(selectSoundButton:) forControlEvents:UIControlEventTouchUpInside];
-    
-    //_soundButton.layer.borderWidth = 1;
-   // _soundButton.backgroundColor = WHITE;
+    [_soundButton addTarget:self action:@selector(selectButton:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:_soundButton];
     
     _spaceButton = [[UIButton alloc] initWithFrame:CGRectMake(_bottomView.frame.origin.x + CGRectGetWidth(_bottomView.frame)/3, _bottomView.frame.origin.y, CGRectGetWidth(_bottomView.frame)/3, CGRectGetHeight(_bottomView.frame))];
     [_spaceButton setImage:[UIImage imageNamed:@"table128"] forState:UIControlStateNormal];
+    [_spaceButton setImage:[UIImage imageNamed:@"table128white"] forState:UIControlStateSelected];
+    _spaceButton.imageView.contentMode = UIViewContentModeCenter;
     [_spaceButton setColor:LIGHT_GRAY_1 forState:UIControlStateNormal];
     [_spaceButton setColor:LIGHT_GRAY_2 forState:UIControlStateHighlighted];
-   // _spaceButton.layer.borderWidth = 1;
-   // _spaceButton.backgroundColor = WHITE;
+    [_spaceButton setColor:GRAY_P forState:UIControlStateSelected];
     [_spaceButton addLeftBorderWithColor:GRAY_M andWidth:1];
     [_spaceButton addTopBorderWithColor:GRAY_M andWidth:1];
     [_spaceButton addBottomBorderWithColor:GRAY_M andWidth:1];
+    [_spaceButton addTarget:self action:@selector(selectButton:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:_spaceButton];
     
     _colorButton = [[UIButton alloc] initWithFrame:CGRectMake(_bottomView.frame.origin.x + 2*CGRectGetWidth(_bottomView.frame)/3, _bottomView.frame.origin.y, CGRectGetWidth(_bottomView.frame)/3, CGRectGetHeight(_bottomView.frame))];
     [_colorButton setImage:[UIImage imageNamed:@"painter128"] forState:UIControlStateNormal];
+    [_colorButton setImage:[UIImage imageNamed:@"painter128white"] forState:UIControlStateSelected];
     [_colorButton setColor:LIGHT_GRAY_1 forState:UIControlStateNormal];
     [_colorButton setColor:LIGHT_GRAY_2 forState:UIControlStateHighlighted];
-
+    [_colorButton setColor:GRAY_P forState:UIControlStateSelected];
     [_colorButton addLeftBorderWithColor:GRAY_M andWidth:1];
     [_colorButton addRightBorderWithColor:GRAY_M andWidth:1];
     [_colorButton addTopBorderWithColor:GRAY_M andWidth:1];
     [_colorButton addBottomBorderWithColor:GRAY_M andWidth:1];
-   // _colorButton.layer.borderWidth = 1;
-   // _colorButton.backgroundColor = WHITE;
+    [_colorButton addTarget:self action:@selector(selectButton:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:_colorButton];
-    
-//    for (NSString *familyName in [UIFont familyNames]){
-//        NSLog(@"Family name: %@", familyName);
-//        for (NSString *fontName in [UIFont fontNamesForFamilyName:familyName]) {
-//            NSLog(@"--Font name: %@", fontName);
-//        }
-//    }
-//    CAShapeLayer* shapeLayer = [CAShapeLayer layer];
-//    CGMutablePathRef path = CGPathCreateMutable();
-    // CGPathMoveToPoint    CGPathAddLines
-   
-    
-//    triangleButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(_bottomView.frame), CGRectGetHeight(_bottomView.frame))];
-//    [triangleButton addTarget:self action:@selector(doThing) forControlEvents:UIControlEventTouchUpInside];
-//
-//    [triangleButton setFrame:CGRectMake(_bottomView.frame.origin.x, _bottomView.frame.origin.y, CGRectGetWidth(_bottomView.frame)/2, CGRectGetHeight(_bottomView.frame))];
-//    [self.view addSubview:triangleButton];
-//    [triangleButton setBackgroundColor:LIGHT_GRAY_1];
-//
-//    CGPathMoveToPoint(path, nil, 0, 0);
-//    CGPathAddLineToPoint(path, nil, CGRectGetWidth(_bottomView.frame)/2, CGRectGetHeight(_bottomView.frame));
-//    CGPathAddLineToPoint(path, nil, 0, CGRectGetHeight(_bottomView.frame));
-//    CGPathCloseSubpath(path);
-//    shapeLayer.path = path;
-//    triangleButton.layer.mask = shapeLayer;
-//    triangleButton.layer.masksToBounds = YES;
-//    NSLog(@"%@", triangleButton);
-    
-    
+    _currentRegion = 1;
+  
+    [_gridCollectionView reloadData];
 }
 
 - (BOOL)selectPathContainingPoint:(CGPoint)point {
@@ -214,7 +202,7 @@
     }
 }
 
-- (void) selectSoundButton:(id)sender {
+- (void) selectButton:(id)sender {
     if ([(UIButton*)sender isSelected]) {
         [(UIButton*)sender setSelected:NO];
     } else {
@@ -222,15 +210,29 @@
     }
 }
 - (void) fillTimer:(id)sender {
+    if (_globalTimer == 0) {
+        [UIView animateWithDuration:1.0 animations:^{
+                [_gridCollectionView reloadData];
+        }];
 
+    }
     _globalTimer += 1;
     [_timerProgressLabel setProgress:_globalTimer/10 timing:TPPropertyAnimationTimingLinear duration:.95 delay:0.0];
     [_timerProgressLabel setText:[NSString stringWithFormat:@"%ld", 11-(long)(_globalTimer)]];
-    NSLog(@"%f", _globalTimer);
-    NSLog(@"%ld",(long)(_globalTimer * 10));
-    if (_globalTimer >= 11) {
-        [(NSTimer*)sender invalidate];
+    if (_globalTimer > 9) {
+        _globalTimer = 0;
+        
+        
+        _currentRegion = arc4random_uniform(8) + 1;
+             [(NSTimer*)sender invalidate];
+        _timer = [NSTimer scheduledTimerWithTimeInterval: 1.0
+                                                  target: self
+                                                selector: @selector(fillTimer:)
+                                                userInfo: nil
+                                                 repeats: YES];
+    
     }
+    
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -238,16 +240,36 @@
 
 #pragma mark - UICollectionViewDelegate
 
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 1;
+}
+
 -(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    NSLog(@"%ld", self.gridAmount)
     return self.gridAmount;
 }
 
--(void)collectionView:(UICollectionView *)collectionView didDeselectItemAtIndexPath:(NSIndexPath *)indexPath{
+-(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
     
 }
 
+-(CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return CGSizeMake(_gridCollectionView.frame.size.width/3, _gridCollectionView.frame.size.height/3);
+}
 -(UICollectionViewCell*)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
-    UICollectionViewCell* cell = [UICollectionViewCell new];
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:_cellIdentifier forIndexPath:indexPath];
+
+    cell.layer.borderWidth = 0.5;
+    cell.layer.borderColor = GRAY_M.CGColor;
+    if (indexPath.row == _currentRegion) {
+        [UIView animateWithDuration:2.0 animations:^{
+             cell.backgroundColor = RED_PEPSI;
+        }];
+       
+    } else {
+        cell.backgroundColor = WHITE;
+    }
+    
     return cell;
 }
 
