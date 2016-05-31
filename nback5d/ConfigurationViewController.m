17cdfedfdae369defd922ffa33fac9358ba28e36
jon-1 <jmm894@gmail.com>
2016-05-30 22:05:13 -0400

    Configuration animations / GameSettings class

--- nback5d/ConfigurationViewController.m
+++ nback5d/ConfigurationViewController.m
@@ -7,37 +7,43 @@
 //
 
 #import "ConfigurationViewController.h"
+#import "Game.h"
+
+const NSInteger spaceBasePts = 250, colorBasePts = 300, soundBasePts = 400;
+const CGFloat penaltyModifier = 3.1;
 
 @interface ConfigurationViewController ()
 
-// Label for nback will update when slider is modified
-@property (weak, nonatomic) IBOutlet UILabel *nbackLabel;
+// Labels for sliders
+@property (weak, nonatomic) IBOutlet UILabel *nbackLabel, *selectionTimeLabel;
 
-// Slider for choosing n
-@property (weak, nonatomic) IBOutlet UISlider *nbackSlider;
+// Sliders for choosing n and time per round
+@property (weak, nonatomic) IBOutlet UISlider *nbackSlider, *timeSlider;
 
-// "selection time" label
-@property (weak, nonatomic) IBOutlet UILabel *selectionTimeLabel;
-
-// Slider for choosing number of seconds per round
-@property (weak, nonatomic) IBOutlet UISlider *timeSlider;
+// stores for slider values
+@property (assign) NSInteger nbackSliderValue, timeSliderValue, nbackSliderOldValue, timeSliderOldValue;
 
 // Labels for switches
 @property (strong, nonatomic) IBOutletCollection(UILabel) NSArray *switchLabels;
 
 // Switches for space, color, or sound - at least one must be on
 @property (strong, nonatomic) IBOutletCollection(UISwitch) NSArray *switches;
-@property (weak, nonatomic) IBOutlet UISwitch *spaceSwitch;
-@property (weak, nonatomic) IBOutlet UISwitch *colorSwitch;
-@property (weak, nonatomic) IBOutlet UISwitch *soundSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *spaceSwitch, *colorSwitch, *soundSwitch;
+
+@property (weak, nonatomic) IBOutlet UILabel *spacePointsLabel, *colorPointsLabel, *soundPointsLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *wrongAnswerPenaltyLabel;
+// swtich for toggling point deductions for wrong answers
+@property (weak, nonatomic) IBOutlet UISwitch *wrongAnswerPenaltySwitch;
 
 // enabled while at least 1 switch on
 @property (weak, nonatomic) IBOutlet UIButton *startButton;
 
+// to play sounds (strong reference required)
+@property (strong, nonatomic) AVAudioPlayer *audioPlayer;
 
-
-@property (assign) BOOL selectionTimeActive;
-@property (assign) BOOL switchesActive;
+// indicates which controls are active
+@property (assign) BOOL selectionTimeActive, switchesActive;
 
 @end
 
@@ -46,67 +52,111 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    
     // disable controls
-    _timeSlider.userInteractionEnabled = false;
+    _timeSlider.userInteractionEnabled = NO;
     
     for (UISwitch* aSwitch in _switches) {
-        aSwitch.userInteractionEnabled = false;
+        aSwitch.userInteractionEnabled = NO;
     }
+    
+    _nbackSliderValue = 1;
+    _timeSliderValue = 1;
+    [self updatePointTotals];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
 
+#pragma mark - Sliders
+
 - (IBAction)nbackSliderValueChanged:(UISlider *)sender {
-    NSInteger value = (NSInteger)sender.value;
+    
+    _nbackSliderValue = (NSInteger)sender.value;
+    
     // Change the nback label to reflect a selected n
-    _nbackLabel.text = [NSString stringWithFormat:@"%ld-BACK", value];
+    _nbackLabel.text = [NSString stringWithFormat:@"%ld-BACK", _nbackSliderValue];
+    
+    if (_nbackSliderValue != _nbackSliderOldValue) {
+        _audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:[NSURL URLWithString:[[NSBundle mainBundle] pathForResource:@"click" ofType:@"wav"]] error:NULL];
+        _audioPlayer.volume = 1.0;
+        [_audioPlayer play];
+    }
+    
+    _nbackSliderOldValue = _nbackSliderValue;
     
     if (!_selectionTimeActive) {
         _selectionTimeActive = true;
         [self performSelector:@selector(enableSelectionTime) withObject:nil afterDelay:0.5];
     }
+    
+    [self updatePointTotals];
 }
 
 - (IBAction)selectionTimeSliderValueChanged:(UISlider *)sender {
-    NSInteger value = (NSInteger)sender.value;
+
+    _timeSliderValue = (NSInteger)sender.value;
+  
     // Change the time label to reflect updated time
+    _selectionTimeLabel.text = [NSString stringWithFormat:@"TIME = %ld SECONDS", _timeSliderValue];
     
-    _selectionTimeLabel.text = [NSString stringWithFormat:@"TIME = %ld SECONDS", value];
+    if (_timeSliderValue != _timeSliderOldValue) {
+        _audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:[NSURL URLWithString:[[NSBundle mainBundle] pathForResource:@"click" ofType:@"wav"]] error:NULL];
+        _audioPlayer.volume = 1.0;
+        [_audioPlayer play];
+    }
     
-    AVAudioPlayer *audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:[NSURL URLWithString:[[NSBundle mainBundle] pathForResource:@"timeslider_2" ofType:@"aiff"]] error:NULL];
-    [audioPlayer play];
-    NSLog(@"%@", [[NSBundle mainBundle] pathForResource:[NSString stringWithFormat:@"timeslider_%ld", value] ofType:@"aiff"])
+    _timeSliderOldValue = _timeSliderValue;
+    
     if (!_switchesActive) {
-        _switchesActive = true;
         [self performSelector:@selector(enableSwitches) withObject:nil afterDelay:0.5];
     }
+    
+    [self updatePointTotals];
 }
 
+#pragma mark - Switches
+
 - (IBAction)spaceSwitchValueChanged:(UISwitch *)sender {
-    
+    [UIView animateWithDuration:0.1
+                     animations:^{ _spacePointsLabel.alpha = _spaceSwitch.on ? 1.0 : 0.05; }];
+
 }
 
 - (IBAction)colorSwitchValueChanged:(UISwitch *)sender {
-    
+    [UIView animateWithDuration:0.1
+                     animations:^{ _colorPointsLabel.alpha = _colorSwitch.on ? 1.0 : 0.05; }];
 }
 
 - (IBAction)soundSwitchValueChanged:(UISwitch *)sender {
-    
+    [UIView animateWithDuration:0.1
+                     animations:^{ _soundPointsLabel.alpha = _soundSwitch.on ? 1.0 : 0.05; }];
 }
+
+- (IBAction)penaltySwitchValueChanged:(UISwitch *)sender {
+    
+    [self updatePointTotals];
+}
+
+
+-(void)testMethodWithString:(NSString*)string andInt:(NSInteger)integer {
+    [self testMethodWithString:@"hi" andInt:3];
+}
+
+#pragma mark - State modification
 
 - (void)enableSelectionTime {
     [UIView animateWithDuration:0.5 animations:^{
         _selectionTimeLabel.alpha = 1.0;
         _timeSlider.alpha = 1.0;
     } completion:^(BOOL finished) {
-        _timeSlider.userInteractionEnabled = true;
+        _timeSlider.userInteractionEnabled = YES;
     }];
 }
 
 - (void)enableSwitches {
+    _switchesActive = YES;
     [UIView animateWithDuration:0.5 animations:^{
         
         for (UILabel* label in _switchLabels) {
@@ -119,9 +169,23 @@
         
     } completion:^(BOOL finished) {
         for (UISwitch* aSwitch in _switches) {
-            aSwitch.userInteractionEnabled = true;
+            aSwitch.userInteractionEnabled = YES;
         }
+        
+        [UIView animateWithDuration:0.5 animations:^{ _wrongAnswerPenaltySwitch.alpha = 1.0;
+            _wrongAnswerPenaltyLabel.alpha = 1.0; }];
     }];
+}
+
+- (void) updatePointTotals {
+    
+    CGFloat modifier = (CGFloat)(_nbackSliderValue * _nbackSliderValue) / (CGFloat)((sqrt(_timeSliderValue)) * 10);
+    
+    modifier *= _wrongAnswerPenaltySwitch.on ? penaltyModifier : 1.0;
+    
+    _spacePointsLabel.text = [NSString stringWithFormat:@"%ld PTS", (NSInteger)(spaceBasePts * modifier)];
+    _colorPointsLabel.text = [NSString stringWithFormat:@"%ld PTS", (NSInteger)(colorBasePts * modifier)];
+    _soundPointsLabel.text = [NSString stringWithFormat:@"%ld PTS", (NSInteger)(soundBasePts * modifier)];
 }
 
 #pragma mark - Navigation
